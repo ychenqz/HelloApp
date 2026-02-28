6b0a597bf16f571e83090d370680331b00595a25
ychenqz <ychenqz@connect.ust.hk>
2026-02-28 17:04:38 +0800

    Update isom5240app2.py

--- isom5240app2.py
+++ isom5240app2.py
@@ -3,7 +3,7 @@ from PIL import Image
 import streamlit as st
 
 # Streamlit UI
-st("Title: Age Classification using ViT")
+st.write("Title: Age Classification using ViT")
 
 # Load the age classification pipeline
 # The code below should be placed in the main part of the program
@@ -15,11 +15,11 @@ image_name = Image.open(image_name).convert("RGB")
 
 # Classify age
 age_predictions = age_classifier(image_name)
-st(age_predictions)
+st.write(age_predictions)
 age_predictions = sorted(age_predictions, key=lambda x: x['score'], reverse=True)
 
 # Display results
-st("Predicted Age Range:")
-st(f"Age range: {age_predictions[0]['label']}")
+st.write("Predicted Age Range:")
+st.write(f"Age range: {age_predictions[0]['label']}")
 
 st.write("Done")
