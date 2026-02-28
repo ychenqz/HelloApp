2f314ed7feeb0c129d830d7d4fab8db44bdc836f
ychenqz <ychenqz@connect.ust.hk>
2026-02-28 17:53:54 +0800

    Update isom5240app2.py

--- isom5240app2.py
+++ isom5240app2.py
@@ -15,18 +15,22 @@ def ageClassifier(imgfilename, modelname):
   age_predictions = age_classifier(image_name)
   return age_predictions
 
-def main():
-  # Streamlit UI
-  st.write("Title: Age Classification using ViT")
-
-  age_predictions = ageClassifier("middleagedMan.jpg", "dima806/fairface_age_image_detection")
+def output(age_predictions):
   st.write(age_predictions)
-  
+  age_predictions = sorted(age_predictions, key=lambda x: x['score'], reverse=True)
   # Display results
   st.write("Predicted Age Range:")
   st.write(f"Age range: {age_predictions[0]['label']}")
   
   st.write("Done")
+
+def main():
+  # Streamlit UI
+  st.write("Title: Age Classification using ViT")
+
+  age_predictions = ageClassifier("middleagedMan.jpg", "dima806/fairface_age_image_detection")
+  output_msg(age_predictions)
+  
 
 # main part
 if __name__ == "__main__":
